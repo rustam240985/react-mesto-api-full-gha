81f0c9055b3bfb0074777e1533869f3e0466a4ae
Rustam Nigamatullin <rust2409@ya.ru>
2023-05-07 15:46:22 +0500

    fix: разрешить cors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const process = require('process');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -16,6 +17,8 @@ process.on('uncaughtException', (err, origin) => {
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 app.use(bodyParser.json());
